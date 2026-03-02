31176a5a220c02edf2d35da03defa01ac2b000b5
Rowan Blackbeard <rowan.blackbeard@yahoo.ca>
2026-03-02 21:24:53 +0100

    clean up some sloppy tailwind

--- front/src/components/about.tsx
+++ front/src/components/about.tsx
@@ -4,8 +4,8 @@ import { Fragment } from 'react';
 function About() {
   return (
     <div className='p-5'>
-      <h2 className='pb-5 border-b-1 border-(--rcolor-gap) inline-block'>
-        <pre className='text-(--rcolor-2) leading-[15px]'>
+      <h2 className='pb-5 border-b border-(--rcolor-gap) inline-block'>
+        <pre className='text-(--rcolor-2) leading-3.75'>
           {`      ___.   .__                 __   ___.                           .___
 ______\\_ |__ |  | _____    ____ |  | _\\_ |__   ____ _____ _______  __| _/
 \\_  __ \\ __ \\|  | \\__  \\ _/ ___\\|  |/ /| __ \\_/ __ \\\\__  \\\\_  __ \\/ __ |
