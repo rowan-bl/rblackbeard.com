bd483b5b18d6bcbf0e25c767a9b192fa8457e5ec
Rowan Blackbeard <rowan.blackbeard@yahoo.ca>
2026-03-02 14:30:36 +0100

    Switch to Astro + Vercel static adapter

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -5,8 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import vercelStatic from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercelStatic({
+    webAnalytics: { enabled: false },
+  }
+  ),
+
   vite: {
     plugins: [tailwindcss()],
   },
